0294513682e9303de2d6ac4f109d42ecf00ddaf5
jarmstong2 <j2armstrong@gmail.com>
2015-11-21 19:14:31 -0500

    fixed inv

--- inverse2.lua
+++ inverse2.lua
@@ -5,7 +5,7 @@ local Inverse, parent = torch.class('nn.Inverse', 'nn.Module')
 function Inverse:updateOutput(input)
     batchSize = input:size()[1]
     dim = input:size()[2]
-    self.output = torch.cudaTensor(batchSize, dim, dim)
+    self.output = torch.CudaTensor(batchSize, dim, dim)
     for i = 1, batchSize do
         inputSize = ((input[i]):size())[1]
         eps = torch.eye(inputSize):cuda() * 1e-1
@@ -17,7 +17,7 @@ end
 function Inverse:updateGradInput(input, gradOutput)
     batchSize = input:size()[1]
     dim = input:size()[2]
-    self.gradInput =  torch.cudaTensor(batchSize, dim, dim)
+    self.gradInput =  torch.CudaTensor(batchSize, dim, dim)
     for i = 1, batchSize do
         inputSize = ((input[i]):size())[1]
         eps = torch.eye(inputSize):cuda() * 1e-1  
