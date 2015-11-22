895c37f7869abdee8a49ee99cfb6aa2f09ba9842
John Armstrong <j2armstrong@gmail.com>
2015-11-22 08:17:54 -0500

    Update checkgradinv.lua

--- checkgradinv.lua
+++ checkgradinv.lua
@@ -16,7 +16,7 @@ inv = nn.Inverse()(m)
 inv_reshape = nn.Reshape(20*20)(inv)
 l2 = nn.Linear(20*20, 10)(inv_reshape)
 
-nng = nn.gModule({input}, {l2})
+nng = nn.gModule({input}, {l2}):cuda()
 
 params, grad_params = nng:getParameters()
 
@@ -29,13 +29,13 @@ function feval(x)
     	end
     	grad_params:zero()
 
-	output = nng:forward(input)
-    doutput = nng:backward(input, torch.ones(2,10))
+	output = nng:forward(input:cuda())
+    doutput = nng:backward(input:cuda(), torch.ones(2,10):cuda())
 
-	return output:sum(), grad_params	
+	return output:sum(), grad_params:double()	
 end
 
-diff, dC, dC_est = optim.checkgrad(feval, params, 1e-2)
+diff, dC, dC_est = optim.checkgrad(feval, params:double(), 1e-2)
 
 print(output)
 print(diff)
